856c9d988b2a151cc642bdaae4f81da0e1575887
sunnyddelight <sunnyddelight@gmail.com>
2016-03-12 15:58:04 -0600

    datapath finished

--- src/mp3.sv
+++ src/mp3.sv
@@ -12,7 +12,14 @@ module mp3
     output lc3b_word pmem_address,
     output logic[127:0] pmem_wdata
 );
-
+logic stall;
+lc3b_word 	mem_address_0, mem_address_1,
+				mem_rdata_0, mem_rdata_1,
+				mem_wdata_0, mem_wdata_1;
+logic mem_resp_0, mem_resp_1,
+		mem_read_0, mem_read_1,
+		mem_write_0, mem_write_1;
+datapath datapath_obj(.*);
 	  
 
 
